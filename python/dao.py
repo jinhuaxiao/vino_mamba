f16aa4530bb8837ee49e26c17a0a3dc32cc44629
yanghua <yanghua1127@gmail.com>
2013-05-03 21:41:25 +0800

    dao

--- python/dao.py
+++ python/dao.py
@@ -20,37 +20,131 @@
 # Date: 4/25/13
 # Time: 11:53 AM
 
+import model
+import pymysql
+import configUtility
+import string
 
-def insert(searchResultList):
-    '''
-        desc:   insert search result list into db
-        args:   list of cse
-        return: none
-    '''
-    pass
 
-def getCSEResult(count=10):
-    '''
-        desc:   get custom search result from db(mysql) [default count item is 10]
-                Attetion: the results records will be deleting and flow into history records
-        args:   number:item count
-        return: list
-    '''
-    pass
+class dataAccessManager:
+    """
+        desc:   data access manager
+    """
 
-def deleteCSEFlowintoHistory(searchResultList):
-    '''
-        desc:   delete (cse) result. Ah, it's not truth!
-                the analysised result will be flowed into history records table
-        args:   list of deleting cse result
-        return: none
-    '''
-    pass
+    def __init__(self):
+        configManager = configUtility.configManager()
+        self.encoder = 'utf8'
+        self.host = configManager.getConfigVal('data_access', 'host')
+        portStr = configManager.getConfigVal('data_access', 'port')
+        self.user = configManager.getConfigVal('data_access', 'user')
+        if portStr:
+            self.port = string.atoi(portStr)
+        else:
+            self.port = 3306
+        self.passwd = configManager.getConfigVal('data_access', 'passwd')
+        if not self.passwd or len(self.passwd) is 0:
+            self.passwd = '123456'
+        self.db = configManager.getConfigVal('data_access', 'db')
 
-def clearHistory():
-    '''
-        desc:   clear all history records
-        args:   none
-        return: none
-    '''
-    pass
+    def insert(self, searchResultList):
+        """
+            desc:   insert search result list into db
+            args:   list of cse
+            return: none
+        """
+        if not searchResultList:
+            raise BaseException, 'the searchResultList can not be none'
+
+        insert_sql = 'INSERT INTO tb_cse_result (title,link,cacheId,snippet,generateTime,keywords) \
+                        VALUES (%(title)s,%(link)s,%(cacheId)s,%(snippet)s,now(),%(keywords)s)'
+
+        conn = pymysql.connect(host=self.host, port=self.port, user=self.user, \
+                               passwd=self.passwd, db=self.db, charset=self.encoder)
+        cur = conn.cursor()
+
+        for item in searchResultList:
+            tmp_dic = dict()
+            tmp_dic['title'] = item.title
+            tmp_dic['link'] = item.link
+            tmp_dic['cacheId'] = item.cacheId
+            tmp_dic['snippet'] = item.snippet
+            tmp_dic['keywords'] = item.keywords
+
+            cur.execute(insert_sql, tmp_dic)
+            pass
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        pass
+
+    def getCSEResult(self, count=10):
+        '''
+            desc:   get custom search result from db(mysql) [default count item is 10]
+                    Attetion: the results records will be deleting and flow into history records
+            args:   number:item count
+            return: list
+        '''
+
+        select_sql="SELECT TOP %d * FROM tb_cse_result ORDER BY cse_id" % count
+
+        conn=pymysql.connect(host=self.host, port=self.port, user=self.user, \
+                               passwd=self.passwd, db=self.db, charset=self.encoder)
+        cur = conn.cursor()
+
+        cur.execute(select_sql)
+        cur.fetchall()
+
+
+        cur.close()
+        conn.close()
+
+        pass
+
+    def deleteCSEFlowintoHistory(self, searchResultList):
+        """
+            desc:   delete (cse) result. Ah, it's not truth!
+                    the analysised result will be flowed into history records table
+            args:   list of deleting cse result
+            return: none
+        """
+        if not searchResultList:
+            raise BaseException,'searchResultList can not be none'
+
+        #TODO: mysql 批量删除
+
+        #TODO:插入历史表
+
+
+        pass
+
+    def insert_history(self,deletedCSEResult):
+        """
+            desc:   insert deleted cse result into cse_history
+        """
+        if not deletedCSEResult:
+            raise BaseException,'deletedCSEResult can not be none'
+
+        insert_sql='INSERT INTO tb_cse_result_history (hy)'
+
+        pass
+
+    def clearHistory(self):
+        """
+            desc:   clear all history records
+            args:   none
+            return: none
+        """
+        deleteAll_sql='DELETE FROM tb_cse_result_history'
+
+        conn=pymysql.connect(host=self.host, port=self.port, user=self.user, \
+                               passwd=self.passwd, db=self.db, charset=self.encoder)
+        cur = conn.cursor()
+
+        cur.execute(deleteAll_sql)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        pass
